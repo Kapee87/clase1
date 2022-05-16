f97c09ece2429433e73f454bc10f908a895b9984
Kapee87 <kapserker@gmail.com>
2022-05-16 19:23:28 -0300

    lo ultimo clase 1

--- src/App.js
+++ src/App.js
@@ -2,24 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
+
+  const nodoJs= (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Bienvenidos a la clase 1</h2>
+        
       </header>
     </div>
   );
+ 
+  console.log(nodoJs);
+  
+  return nodoJs;
 }
 
 export default App;
+
